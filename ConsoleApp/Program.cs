70b6defbb156c747122bb83e32e61f0e3053777a
dotadima191@gmail.com <dotadima191@gmail.com>
2023-02-02 13:00:03 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using Plugin;
+namespace ConsoleApp
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PluginOne plugin = new PluginOne();
+            plugin.RunLogic("Метод библиотеки Plugin, PluginOne");
+        }
+    }
+}
---
+++ Plugin/PluginOne.cs
@@ -0,0 +1,12 @@
+﻿using System.Threading.Channels;
+
+namespace Plugin
+{
+    public class PluginOne
+    {
+        public  void RunLogic(string message) 
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
